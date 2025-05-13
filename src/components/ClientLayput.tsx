f4fb913866a6ae18a46b9e7f856e907e7266a2b8
Yash Raj <kyashraj991@gmail.com>
2025-05-13 23:52:58 +0530

    create login page

--- src/components/ClientLayput.tsx
+++ src/components/ClientLayput.tsx
@@ -14,7 +14,7 @@ const ClientLayput = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen dark:bg-dark bg-light text-dark dark:text-light transition-all">
       {showSidebar && <Sidebar />}
-      <div className="flex flex-col flex-grow w-full max-w-[1400px]">
+      <div className="flex flex-col flex-grow">
         {showNavbar && <Navbar />}
         <main className="flex-1 overflow-y-auto">{children}</main>
       </div>
