70e2ed7c570c7e61dec6e30e48c3df367593b6df
Yash Raj <kyashraj991@gmail.com>
2025-06-06 20:44:12 +0530

    add GET endpoint for retrieving a post by ID

---
+++ src/app/api/posts/[postId]/route.ts
@@ -0,0 +1,28 @@
+  import prisma from '@/lib/prisma';
+  import { NextRequest, NextResponse } from 'next/server';
+
+  export async function GET(
+    req: NextRequest,
+    { params }: { params: { postId: string } }
+  ) {
+    const { postId } = await params;
+
+    try {
+
+      const post = await prisma.post.findUnique({
+        where: {
+          id: postId,
+        },
+      });
+
+      if (!post) {
+        return NextResponse.json({ message: 'POST NOT FOUND!' }, { status: 404 });
+      }
+
+      return NextResponse.json({ response: post }, { status: 200 });
+      
+    } catch (error) {
+      console.error('Error getting the posts', error);
+      return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+    }
+  }
