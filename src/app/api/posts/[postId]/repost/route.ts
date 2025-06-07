d194632ee327dff8022ae6b63af41e436d6b4d8e
Yash Raj <kyashraj991@gmail.com>
2025-06-07 12:42:18 +0530

    add POST and DELETE endpoints for reposting and removing reposts

---
+++ src/app/api/posts/[postId]/repost/route.ts
@@ -0,0 +1,97 @@
+import { getUserByToken } from '@/lib/getUser';
+import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { postId: string } }
+) {
+  try {
+    const { postId } = await params;
+
+    const user = await getUserByToken(req);
+
+    if (!user) {
+      return NextResponse.json(
+        { message: 'User not authorize' },
+        { status: 401 }
+      );
+    }
+
+    const userId = user.id;
+
+    const existingRepost = await prisma.repost.findUnique({
+      where: {
+        userId_postId: {
+          userId,
+          postId,
+        },
+      },
+    });
+
+    if (existingRepost) {
+      return NextResponse.json(
+        { message: 'Already reposted' },
+        { status: 400 }
+      );
+    }
+
+    const repost = await prisma.repost.create({
+      data: {
+        userId,
+        postId,
+      },
+    });
+
+    return NextResponse.json({
+      response: repost,
+    });
+  } catch (error) {
+    console.error('Error reposting', error);
+    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { postId: string } }
+) {
+  const { postId } = await params;
+
+  try {
+    const user = await getUserByToken(req);
+
+    if (!user) {
+      return NextResponse.json(
+        { message: 'User not authorized' },
+        { status: 401 }
+      );
+    }
+
+    const userId = user.id;
+
+    const deleteRepost = await prisma.repost.delete({
+      where: {
+        userId_postId: {
+          userId,
+          postId,
+        },
+      },
+    });
+
+    return NextResponse.json(
+      { message: 'Repost removed successfullt' },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error === 'P2025') {
+      return NextResponse.json(
+        { message: 'Repost not found' },
+        { status: 404 }
+      );
+    }
+
+    console.error('Error Deleting repost', error);
+    return NextResponse.json({ message: 'Server' }, { status: 500 });
+  }
+}
