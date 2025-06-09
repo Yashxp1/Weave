6866ee55f62d8c7ab13a45c684279a3dc3f2fe70
Yash Raj <kyashraj991@gmail.com>
2025-06-09 13:49:20 +0530

    implement GET endpoint for retrieving following users

--- src/app/api/users/[userId]/follow/route.ts
+++ src/app/api/users/[userId]/follow/route.ts
@@ -29,7 +29,6 @@ export async function POST(
     });
 
     if (existingFollow) {
-    
       await prisma.follow.delete({
         where: {
           followerId_followingId: {
@@ -41,7 +40,6 @@ export async function POST(
 
       return NextResponse.json({ message: 'Unfollowed user' }, { status: 200 });
     } else {
-    
       await prisma.follow.create({
         data: {
           followerId: userId,
@@ -54,5 +52,59 @@ export async function POST(
   } catch (error) {
     console.error('Error following/unfollowing user', error);
     return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+  }
+}
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { targetUserId: string } }
+) {
+  const { targetUserId } = params;
+
+  try {
+    const user = await getUserByToken(req);
+    if (!user || user.id !== targetUserId) {
+      return NextResponse.json(
+        { message: 'USER NOT AUTHORIZED' },
+        { status: 401 }
+      );
+    }
+    const userId = user.id;
+
+    const followingUsers = await prisma.follow.findMany({
+      where: {
+        followerId: targetUserId,
+      },
+      select: {
+        following: {
+          select: {
+            id: true,
+            name: true,
+            profilePic: true,
+            posts: {
+              select: {
+                id: true,
+                content: true,
+                image: true,
+                createdAt: true,
+              },
+            },
+            _count: {
+              select: {
+                followers: true,
+                following: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const cleanFollowing = followingUsers.map((f) => f.following);
+
+    return NextResponse.json({ following: cleanFollowing }, { status: 200 });
+  } catch (error) {
+    console.error('Error getting followers', error);
+    return NextResponse.json({ message: 'Sever Error' }, { status: 500 });
   }
 }
