1717267b5e07e0d5628d282cece7b93f4c14ba8a
Yash Raj <kyashraj991@gmail.com>
2025-06-30 17:03:08 +0530

    fix: correct indentation and improve response message for fetching a single post

--- src/app/api/posts/[postId]/route.ts
+++ src/app/api/posts/[postId]/route.ts
@@ -1,28 +1,29 @@
-  import prisma from '@/lib/prisma';
-  import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
 
-  export async function GET(
-    req: NextRequest,
-    { params }: { params: { postId: string } }
-  ) {
-    const { postId } = await params;
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { postId: string } }
+) {
+  const { postId } = await params;
 
-    try {
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+    });
 
-      const post = await prisma.post.findUnique({
-        where: {
-          id: postId,
-        },
-      });
-
-      if (!post) {
-        return NextResponse.json({ message: 'POST NOT FOUND!' }, { status: 404 });
-      }
-
-      return NextResponse.json({ response: post }, { status: 200 });
-      
-    } catch (error) {
-      console.error('Error getting the posts', error);
-      return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+    if (!post) {
+      return NextResponse.json({ message: 'POST NOT FOUND!' }, { status: 404 });
     }
+
+    return NextResponse.json(
+      { message: 'Signle post fetched', response: post },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error getting the posts', error);
+    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
   }
+}
