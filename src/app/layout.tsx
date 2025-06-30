d82b26fd7239c2c9afa422296a9d5e961dbb91ff
Yash Raj <kyashraj991@gmail.com>
2025-06-30 16:00:31 +0530

    cleanup: old ui reset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,34 @@
-  import type { Metadata } from 'next';
-  import { Geist, Geist_Mono } from 'next/font/google';
-  import './globals.css';
-  import { ThemeProvider } from '@/contexts/ThemeContext';
-  import { ModalProvider } from '@/contexts/ModalContext';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
-  import ClientLayout from '@/components/layout/ClientLayout';
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
 
-  const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-  });
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
 
-  const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
-  });
+export const metadata: Metadata = {
+  title: 'weave',
+  description: 'Share your thoughts',
+};
 
-  export const metadata: Metadata = {
-    title: 'weave',
-    description: 'Share your thoughts',
-  };
-
-  export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <ThemeProvider>
-            <ModalProvider>
-              <ClientLayout>{children}</ClientLayout>
-            </ModalProvider>
-          </ThemeProvider>
-        </body>
-      </html>
-    );
-  }
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,9 @@
-import Modal from '@/components/layout/Modal';
-import Link from 'next/link';
-import React from 'react';
+import React from 'react'
 
 const page = () => {
   return (
-    <div className="text-xl space-x-4">
-      <Link href="/login">
-        <button className=" bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100">
-          LOGIN
-        </button>
-      </Link>
-      <Link href="/register">
-        <button className=" bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100">
-          Register
-        </button>
-      </Link>
+    <div className='text-4xl'>LANDING PAGE</div>
+  )
+}
 
-      <Link href="/home">
-        <button className=" bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100">
-          Home
-        </button>
-      </Link>
-    </div>
-  );
-};
-
-export default page;
+export default page
