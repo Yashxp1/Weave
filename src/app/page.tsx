f350121659fdb45ceca7a671433a2320d76ab53e
Yash Raj <kyashraj991@gmail.com>
2025-06-30 17:04:07 +0530

    feat: add shadcn ui and implement dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { ModeToggle } from '@/components/ModeToggle';
+import React from 'react';
 
 const page = () => {
   return (
-    <div className='text-4xl'>LANDING PAGE</div>
-  )
-}
+    <div className="flex justify-between p-3">
+      <h1>Landing page</h1>
+      <div>
+        <ModeToggle />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
