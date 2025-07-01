ec02e97e8e8440d4855085209d59dfd0cdc24fa6
Yash Raj <kyashraj991@gmail.com>
2025-07-01 10:45:33 +0530

    feat: enhance authentication store with login functionality and improve registration flow

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -2,6 +2,8 @@ import { create } from 'zustand';
 import { toast } from 'react-hot-toast';
 import axios from 'axios';
 import { redirect } from 'next/navigation';
+import { promises } from 'dns';
+import { date } from 'zod';
 
 const basedURL = 'http://localhost:3000/api';
 
@@ -13,32 +15,58 @@ type AuthUser = {
 
 type RegisterData = {
   name: string;
-  email: String;
+  email: string;
+  password: string;
+};
+
+type LoginData = {
+  email: string;
   password: string;
 };
 
 type AuthStore = {
   authUser: AuthUser | null;
   isRegistering: boolean;
+  isLoggingIN: boolean;
 
-  register: (data: RegisterData) => Promise<void>;
+  register: (data: RegisterData) => Promise<boolean>;
+  login: (data: LoginData) => Promise<boolean>;
 };
 
 export const useAuthStore = create<AuthStore>((set, get) => ({
   authUser: null,
   isRegistering: false,
+  isLoggingIN: false,
 
-  register: async (data: RegisterData) => {
+  register: async (data) => {
     set({ isRegistering: true });
     try {
       const res = await axios.post<AuthUser>(`${basedURL}/auth/register`, data);
       set({ authUser: res.data });
       toast.success('Account created successfully');
+      return true;
     } catch (error: any) {
       toast.error(error.response?.data?.message || 'Registration failed');
+      return false;
     } finally {
-      redirect('/dashboard')
       set({ isRegistering: false });
+
+      // redirect('/dashboard');
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIN: true });
+    try {
+      const res = await axios.post<AuthUser>(`${basedURL}/auth/login`, data);
+      set({ authUser: res.data });
+      toast.success('Login successful');
+      return true;
+    } catch (error: any) {
+      toast.error(error.response?.data?.message || 'Login failed');
+      return false;
+    } finally {
+      set({ isLoggingIN: false });
     }
   },
 }));
