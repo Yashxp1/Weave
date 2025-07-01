faf2bedde8bec7d1f28d4849208c0f3787a97f18
Yash Raj <kyashraj991@gmail.com>
2025-07-01 10:47:48 +0530

    feat: enhance validation messages in authentication schemas for better user feedback

--- src/schema/Auth.ts
+++ src/schema/Auth.ts
@@ -1,12 +1,18 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const Loginschema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters' }),
 });
 
 export const Registerschema = z.object({
-  name: z.string().min(2),
-  email: z.string().email(),
-  password: z.string().min(6),
+  name: z
+    .string()
+    .min(2, { message: 'Name must be at least 2 characters long' }),
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters' }),
 });
