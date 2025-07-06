e155341384209ff7e7f59968dbd0476f3bf4054a
Yash Raj <kyashraj991@gmail.com>
2025-07-06 15:41:39 +0530

    feat: add Chat component and ScrollArea for enhanced messaging interface

---
+++ src/app/messages/page.tsx
@@ -0,0 +1,13 @@
+
+import ChatComponent from '@/components/Chat';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <ChatComponent />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/Chat.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Chat = () => {
+  return <div>CHAT PAGE</div>;
+};
+
+export default Chat;
