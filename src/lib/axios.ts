4ba6c9d30dc81ddfcdd665829400568edf6eec81
Yash Raj <kyashraj991@gmail.com>
2025-07-08 21:59:55 +0530

    feat: add axios instance configuration file

---
+++ src/lib/axios.ts
@@ -0,0 +1,11 @@
+// import axios, { AxiosInstance } from 'axios';
+
+// const baseURL: string =
+//   import.meta.env.MODE === 'development'
+//     ? 'http://localhost:3000/api'
+//     : 'http://localhost:3000/api';
+
+// export const axiosInstance: AxiosInstance = axios.create({
+//   baseURL,
+//   withCredentials: true,
+// });
